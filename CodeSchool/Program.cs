119ff67b9e372c4e94400227547006c410d6ad09
David Oku <84320460+MasterOfLogic1@users.noreply.github.com>
2023-01-22 19:30:22 -0500

    first commit

---
+++ CodeSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿// MOL Tutorials
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userName;
+        string userAge;
+        Console.WriteLine("Please provide your name below:");
+        userName = Console.ReadLine();
+        Console.WriteLine("Please "+userName+" provide your age below:");
+        userAge = Console.ReadLine();
+        string messageResponse = "Hello "+userName+", You are "+userAge+" years old";
+        Console.WriteLine(messageResponse);
+
+
+        
+    }
+
+
+}
